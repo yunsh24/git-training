d84724848a0516791ff058a20ccd86d8d1d31ce9
yunsh24 <punch01@naver.com>
2016-03-30 17:08:59 +0900

    packing knapsack : rename packing
    Signed-off-by: yunsh24 <punch01@naver.com>

---
+++ commit_5/packing_knapsack.c
@@ -0,0 +1,35 @@
+/*
+ * Packing knapsack question
+ *
+ * Copyright (C) 2016, Taeung Song <treeze.taeung@gmail.com>
+ *
+ * !! EDIT HERE!!
+ */
+#include <stdio.h>
+
+struct knapsack {
+	unsigned int limited_wgt;
+	unsigned int max_price;
+};
+
+struct jewelry {
+	unsigned int wgt;
+	unsigned int price;
+};
+
+void pack_knapsack(struct knapsack *knapsack_list,
+		      struct jewelry **usable_jewels)
+{
+	/* Case by case pack knapsack following maximum
+	 * price per limited weight.
+	 */
+}
+
+int get_maxprice(struct knapsack *knapsack_list, struct jewelry *jewelry)
+{
+	/* Get maximum price based on limited weight */
+}
+
+int main(int argc, const char **argv)
+{
+}
